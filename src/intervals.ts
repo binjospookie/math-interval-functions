e31872c1f639947a47d8561d1ad21d5de7c657f7
Viktor Pasynok <binjospookie@gmail.com>
2020-04-15 17:55:50 +0300

    feat: wip

---
+++ src/intervals.ts
@@ -0,0 +1,6 @@
+export const opened = /^\(([0-9-.]*),([0-9-.]*)\)$/;
+export const closed = /^\[([0-9-.]*),([0-9-.]*)\]$/;
+export const leftOpen = /^\(([0-9-.]*),([0-9-.]*)\]$/;
+export const leftClosed = /^\[([0-9-.]*),([0-9-.]*)\)$/;
+
+// console.log("[-123.4,-10)".match(leftClosed));
