e5e48fa8769d9b4e07a4d696538c9c8df076eb66
Viktor Pasynok <binjospookie@gmail.com>
2020-04-16 12:35:51 +0300

    perf: faster and smaller

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import { intervalsList } from './services/intervals';
+const intervalsList: readonly {
+  readonly comparator: (x: { readonly max: number; readonly min: number; readonly value: number }) => boolean;
+  readonly regex: RegExp;
+}[] = [
+  {
+    comparator: ({ value, min, max }) => value > min && value < max,
+    regex: /^\(([0-9-.]*),([0-9-.]*)\)$/,
+  },
+  {
+    comparator: ({ value, min, max }) => value >= min && value <= max,
+    regex: /^\[([0-9-.]*),([0-9-.]*)\]$/,
+  },
+  {
+    comparator: ({ value, min, max }) => value > min && value <= max,
+    regex: /^\(([0-9-.]*),([0-9-.]*)\]$/,
+  },
+  {
+    comparator: ({ value, min, max }) => value >= min && value < max,
+    regex: /^\[([0-9-.]*),([0-9-.]*)\)$/,
+  },
+];
 
 export const inInterval = (interval: string, value: number) =>
-  intervalsList.reduce((acc, { regex, comparator }) => {
-    const matchResult = interval.match(regex);
+  intervalsList.some(({ regex, comparator }) => {
+    const matchResult = regex.exec(interval);
 
     if (!matchResult) {
-      return acc;
+      return false;
     }
 
     const [match, min, max] = matchResult;
@@ -15,4 +35,4 @@ export const inInterval = (interval: string, value: number) =>
       min: !min ? -Infinity : ~~min,
       value,
     });
-  }, false);
+  });
--- src/services/intervals/constants.ts
+++
@@ -1,4 +0,0 @@
-export const opened = /^\(([0-9-.]*),([0-9-.]*)\)$/;
-export const closed = /^\[([0-9-.]*),([0-9-.]*)\]$/;
-export const leftOpen = /^\(([0-9-.]*),([0-9-.]*)\]$/;
-export const leftClosed = /^\[([0-9-.]*),([0-9-.]*)\)$/;
